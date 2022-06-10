8e72d590517b65f5c6d6e810033d1477df1b8836
abhinavthota01 <abhinavthota01@gmail.com>
2022-06-10 00:03:47 -0700

    worked on background.js scripts which communicates with contentScript.js

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters); // searchParams - interface to interact with URLs
+    //send message to contentScript that new video has been opened
+    //and send the unique video id from the url to contentScript
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v"), //gets everything after "v=" in the url
+    })
+  }
+})
